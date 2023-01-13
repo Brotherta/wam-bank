1b85393ce9061aad50efb8ffe6eca082c4aa6529
Antoine Vidal-Mazuy <antoine.vidalma@gmail.com>
2023-01-13 10:53:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const plugins = require("./plugins.json");
+
+const app = express();
+const port = process.env.PORT || 3008;
+
+app.use(cors());
+
+// app.enable("trust proxy");
+
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Resource-Policy", "cross-origin");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.get("/wams", (req, res) => {
+  res.send(plugins.map((plugin) => `${req.protocol}://${req.get("Host")}/plugins/${plugin}/`));
+});
+
+app.use("/", express.static(path.join(__dirname, "./PedalBoard")));
+
+app.use("/plugins", express.static(path.join(__dirname, "./plugins")));
+
+app.listen(port, () => {
+  console.log(`App working on http://localhost:${port}`);
+});
